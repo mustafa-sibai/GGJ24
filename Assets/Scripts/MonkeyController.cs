ee5094e98fb6f7ddd4528364ec0d6485d8e790a6
Mustafa Sibai <contact@m-sibai.com>
2024-01-27 18:41:33 +0400

    He is rolling and aiming balling

--- Assets/Scripts/MonkeyController.cs
+++ Assets/Scripts/MonkeyController.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class MonkeyController : MonoBehaviour
 {
-    Animator animator;
     [SerializeField] int playerNumber;
     [SerializeField] float maxSpeed;
+    [SerializeField] GameObject aimingSphere;
+    [SerializeField] float aimRadius;
+
+    Animator animator;
+    Vector3 rightThumbStickLastPosition;
     float speed;
 
     void Start()
     {
         animator = GetComponent<Animator>();
     }
-    float angle;
+
     void Update()
     {
+        //Movement Code
         float horizontal = Input.GetAxis($"Horizontal-{playerNumber}");
         float vertical = Input.GetAxis($"Vertical-{playerNumber}");
 
         if (Mathf.Abs(horizontal) > 0 || Mathf.Abs(vertical) > 0f)
         {
-            angle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, angle, 0);
+            float leftThumbStickAngle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, leftThumbStickAngle, 0);
 
             animator.SetBool("Walk", true);
             speed = maxSpeed;
@@ -33,37 +38,26 @@ public class MonkeyController : MonoBehaviour
             speed = 0;
         }
 
-        print($"hor: {horizontal} ver: {vertical}");
-
-
-        //print("xx" + );
-
-
-        //if (Input.GetAxisRaw("RightTrigger") > 0)
-        {
-
-
-        }
-
-
-        {
-            //
-
-        }
-
-        /*{
-            //speed = maxSpeed * vertical;
-            print(speed);
-
-        }
-        {
-            animator.SetBool("Walk", false);
-            speed = 0;
-        }*/
-
-        transform.position += 
+        transform.position +=
             (Vector3.forward * vertical * speed +
             Vector3.right * horizontal * speed)
             * Time.deltaTime;
+
+
+        //Aiming Code
+        float rightThumbStickHorizontal = Input.GetAxis($"RightThumbStickHorizontal-{playerNumber}");
+        float rightThumbStickVertical = Input.GetAxis($"RightThumbStickVertical-{playerNumber}");
+
+        float rightThumbStickAngle = Mathf.Atan2(rightThumbStickVertical, rightThumbStickHorizontal);
+
+        float x = Mathf.Cos(rightThumbStickAngle) * aimRadius;
+        float y = rightThumbStickLastPosition.y;
+        float z = Mathf.Sin(rightThumbStickAngle) * aimRadius;
+
+        if (Mathf.Abs(rightThumbStickHorizontal) > .75 || Mathf.Abs(rightThumbStickVertical) > .75)
+        {
+            aimingSphere.transform.position = transform.position + new Vector3(x, y, z);
+            rightThumbStickLastPosition = aimingSphere.transform.position;
+        }
     }
 }
