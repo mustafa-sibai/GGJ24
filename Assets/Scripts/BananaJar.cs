5a6e470b0c9ad3d5c35510ec76978b7c014a6fb7
Anna Tookey <40760387+OokeyTookey@users.noreply.github.com>
2024-01-27 09:44:23 -0800

    Working on BananaCage

--- Assets/Scripts/BananaJar.cs
+++ Assets/Scripts/BananaJar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BananaJar : MonoBehaviour
 {
@@ -10,36 +11,47 @@ public class BananaJar : MonoBehaviour
     public Sprite yesTakeBananas;
     private bool enteredBananaJar = false;
 
+    [SerializeField] GameObject popUpBox;
+    [SerializeField] GameObject numberOfBananasBox;
+    [SerializeField] TMP_Text popUpBoxText;
+    [SerializeField] TMP_Text numberOfBananasText;
+    private string currentText;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             enteredBananaJar = true;
-          
-
-            
+            numberOfBananasBox.SetActive(true);
+            popUpBox.SetActive(true);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         enteredBananaJar = false;
-
+        numberOfBananasBox.SetActive(false);
+        popUpBox.SetActive(false);
     }
 
     private void Update()
     {
-        Debug.Log("Total banana count " + currentNumberOfBananas);
-        if (Input.GetButtonDown("Fire3"))
+        popUpBoxText.text = currentText;
+        numberOfBananasText.text = currentNumberOfBananas.ToString() + " / " + totalNumberOfBananas.ToString();
+        if (currentNumberOfBananas > 0 )
         {
-            if (currentNumberOfBananas > 0 && enteredBananaJar)
+            currentText = "Press X to Pickup";
+            if (Input.GetButtonDown("Fire3") && enteredBananaJar)
             {
                 currentNumberOfBananas--;
             }
+        }
 
-            if (currentNumberOfBananas <= 0)
-            {
-            }
-        };
+        if (currentNumberOfBananas <= 0)
+        {
+            currentText = "NO MORE BANANAS";
+        }
+          
     }
+
 }
